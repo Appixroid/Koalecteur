f9fee7fd5223451a9d9a52a8445fb8fa8b0900fb
valliere <eddie.vallier@etu.umontpellier.fr>
2019-01-30 13:42:02 +0100

    Template update

--- templates/lib/date.php
+++ templates/lib/date.php
@@ -3,7 +3,7 @@
 		$days = array('Mercredi', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
 		$mouths = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
 		$today = getdate();
-		$str = $days[$today['wday']] . " " . $today['mday'] . " " . $mouths[$today['mon']] . ", " . $today['year'];
+		$str = $days[$today['wday']] . " " . $today['mday'] . " " . $mouths[$today['mon']-1] . ", " . $today['year'];
 		return $str;
 	}
 ?>
